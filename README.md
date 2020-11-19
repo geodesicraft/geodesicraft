e8c62cf6db15d7fb7175b65407ace3aa303b6ea2
jonthesquirrel <jdh11235@gmail.com>
2020-11-19 06:33:41 -0800

    Hook into WorldEdit API as dependency

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ Geodesic Dome Maker for Minecraft
 
 ***(Currently in development)***
 
+## Plugin installation
+
+### Spigot/Bukkit/Paper:
+
+***(Geodesicraft plugin downloads coming soon)***
+
+Requires [**WorldEdit**](https://enginehub.org/worldedit/) plugin (at least version 7.2) 
+
 ## Local development
 1. Install JDK (at least version 8), an IDE (IntelliJ recommended), and git 
 2. Clone the repository
--- src/main/java/io/github/geodesicraft/geodesicraft/CommandGeo.java
+++ src/main/java/io/github/geodesicraft/geodesicraft/CommandGeo.java
@@ -1,5 +1,6 @@
 package io.github.geodesicraft.geodesicraft;
 
+import com.sk89q.worldedit.WorldEdit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,7 +9,7 @@ public class CommandGeo implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        System.out.println("//geo has been run");
+        System.out.println("Geodesicraft: Worldedit " + WorldEdit.getVersion() + " has been hooked into! Yay!");
         return false;
     }
 
